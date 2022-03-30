4f9ad19a7138a83bf72f8ea0d1514620b5459d2c
fabseman <4969281-FabseGP02@users.noreply.gitlab.com>
2022-03-30 13:19:19 +0000

    Fixed starting services with sway

--- scripts/artix/autostart.sh
+++ scripts/artix/autostart.sh
@@ -1,33 +1,54 @@
 #!/bin/sh
 
-  if [[ "$1" == "river" ]]; then
-    command="riverctl spawn"
-  elif [[ "$1" == "sway" ]]; then
-    command="exec"
-  fi
-  for service in yambar i3status-rust foot pipewire pipewire-pulse wireplumber mako syncthing wl-paste; do
-    if ! [[ $(pidof $service) ]]; then
-      if [[ "$service" == "foot" ]]; then
-        $command "foot --server"
-      elif [[ "$service" == "wl-paste" ]]; then
-        $command "wl-paste -t text --watch clipman store"
-      elif [[ "$service" == "wlsunset" ]]; then
-        $command "wlsunset -l 55.27023 -L 9.90081"
-      elif [[ "$service" == "yambar" ]]; then
-        if [[ "$1" == "river" ]]; then
-          $command $service
+  for service in yambar foot pipewire pipewire-pulse wireplumber mako syncthing wl-paste; do
+    if [[ "$1" == "sway" ]]; then
+      rm -rf /home/fabse/.config/sway/{exec,terminal}
+      touch /home/fabse/.config/sway/{exec,terminal}
+      if ! [[ $(pidof $service) ]]; then
+        if [[ "$service" == "foot" ]]; then
+          cat << EOF | tee -a /home/fabse/.config/sway/exec > /dev/null
+exec foot --server
+EOF
+          cat << EOF | tee -a /home/fabse/.config/sway/terminal > /dev/null
+set \$term footclient
+EOF
+        elif [[ "$service" == "wl-paste" ]]; then
+          cat << EOF | tee -a /home/fabse/.config/sway/exec > /dev/null
+exec wl-paste -t text --watch clipman store
+EOF
+        elif [[ "$service" == "wlsunset" ]]; then
+          cat << EOF | tee -a /home/fabse/.config/sway/exec > /dev/null
+exec wlsunset -l 55.27023 -L 9.90081
+EOF
+        elif [[ "$service" == "syncthing" ]]; then
+          cat << EOF | tee -a /home/fabse/.config/sway/exec > /dev/null
+exec syncthing serve --no-browser
+EOF
+        elif [[ "$service" == "yambar" ]]; then
+          :
+        else
+          cat << EOF | tee -a /home/fabse/.config/sway/exec > /dev/null
+exec $service
+EOF
         fi
-      elif [[ "$service" == "i3status-rust" ]]; then
-        if [[ "$1" == "sway" ]]; then
-          $command $service
-        fi
-      elif [[ "$service" == "syncthing" ]]; then
-        syncthing_start="syncthing serve --no-browser"
-        $command $syncthing_start
-      else
-        $command $service
+      elif [[ $(pidof foot) ]]; then
+        cat << EOF | tee -a /home/fabse/.config/sway/terminal > /dev/null
+set \$term foot
+EOF
       fi
-    elif [[ $(pidof foot) ]] && [[ "$1" == "sway" ]]; then
-      set $term footclient
+    elif [[ "$1" == "river" ]]; then
+      if ! [[ $(pidof $service) ]]; then
+        if [[ "$service" == "foot" ]]; then
+          riverctl spawn "foot --server"
+        elif [[ "$service" == "wl-paste" ]]; then
+          riverctl spawn "wl-paste -t text --watch clipman store"
+        elif [[ "$service" == "wlsunset" ]]; then
+          riverctl spawn "wlsunset -l 55.27023 -L 9.90081"
+        elif [[ "$service" == "syncthing" ]]; then
+          riverctl spawn "syncthing serve --no-browser"
+        else
+          riverctl spawn $service
+        fi
+      fi
     fi
   done

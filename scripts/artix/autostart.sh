673dc0a13169b4e095fe833d2c6b15f68487888c
fabseman <4969281-FabseGP02@users.noreply.gitlab.com>
2022-03-31 10:41:17 +0000

    Improved autostart

--- scripts/artix/autostart.sh
+++ scripts/artix/autostart.sh
@@ -1,34 +1,32 @@
 #!/bin/sh
 
+  if [[ "$1" == "sway" ]]; then
+    rm -rf /home/fabse/.config/sway/{exec,terminal}
+    touch /home/fabse/.config/sway/{exec,terminal}
+  fi
   for service in yambar foot pipewire pipewire-pulse wireplumber mako syncthing wl-paste; do
     if [[ "$1" == "sway" ]]; then
-      rm -rf /home/fabse/.config/sway/{exec,terminal}
-      touch /home/fabse/.config/sway/{exec,terminal}
       if ! [[ $(pidof $service) ]]; then
         if [[ "$service" == "foot" ]]; then
           cat << EOF | tee -a /home/fabse/.config/sway/exec > /dev/null
-exec foot --server
+exec_always foot --server
 EOF
           cat << EOF | tee -a /home/fabse/.config/sway/terminal > /dev/null
 set \$term footclient
 EOF
         elif [[ "$service" == "wl-paste" ]]; then
           cat << EOF | tee -a /home/fabse/.config/sway/exec > /dev/null
-exec wl-paste -t text --watch clipman store
-EOF
-        elif [[ "$service" == "wlsunset" ]]; then
-          cat << EOF | tee -a /home/fabse/.config/sway/exec > /dev/null
-exec wlsunset -l 55.27023 -L 9.90081
+exec_always wl-paste -t text --watch clipman store
 EOF
         elif [[ "$service" == "syncthing" ]]; then
           cat << EOF | tee -a /home/fabse/.config/sway/exec > /dev/null
-exec syncthing serve --no-browser
+exec_always syncthing serve --no-browser
 EOF
         elif [[ "$service" == "yambar" ]]; then
           :
         else
           cat << EOF | tee -a /home/fabse/.config/sway/exec > /dev/null
-exec $service
+exec_always $service
 EOF
         fi
       elif [[ $(pidof foot) ]]; then
@@ -36,14 +34,13 @@ EOF
 set \$term foot
 EOF
       fi
+      chmod u+x /home/fabse/.config/sway/exec
     elif [[ "$1" == "river" ]]; then
       if ! [[ $(pidof $service) ]]; then
         if [[ "$service" == "foot" ]]; then
           riverctl spawn "foot --server"
         elif [[ "$service" == "wl-paste" ]]; then
           riverctl spawn "wl-paste -t text --watch clipman store"
-        elif [[ "$service" == "wlsunset" ]]; then
-          riverctl spawn "wlsunset -l 55.27023 -L 9.90081"
         elif [[ "$service" == "syncthing" ]]; then
           riverctl spawn "syncthing serve --no-browser"
         else

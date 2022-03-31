3ac393e596d63cb197a3c4028efd89ab54866672
fabseman <4969281-FabseGP02@users.noreply.gitlab.com>
2022-03-31 10:36:19 +0000

    Updated screensaver

--- .config/sway/screensaver.sh
+++ .config/sway/screensaver.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
   mv /home/fabse/.config/swaylock/config /home/fabse/.config/swaylock/tmp
-  term='alacritty -t Screensaver -e'
-  saver=cmatrix
-  locker='swaylock -c 00000000'
-  ($term $saver) &
+  random=$(shuf -i 0-10 -n 1)
+  term="footclient -T Screensaver"
+  saver="pipes -t $random -R"
+  locker="swaylock -c 00000000"
+  ($term $saver ) &
   sleep .2
   ($locker && kill $!)
   mv /home/fabse/.config/swaylock/tmp /home/fabse/.config/swaylock/config

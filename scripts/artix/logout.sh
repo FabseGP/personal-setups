bc885c047ea59b33e264b81cc8f0328cb4d469f5
fabseman <4969281-FabseGP02@users.noreply.gitlab.com>
2022-03-31 08:02:47 +0000

    Uploaded initial logout-script

---
+++ scripts/artix/logout.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+  for service in yambar foot pipewire pipewire-pulse wireplumber mako wl-paste sunpaper sworkstyle syncthing; do
+    if [[ $(pidof $service) ]]; then
+      killall $service
+    fi
+  done
+  if [[ "$1" == "sway" ]]; then
+    swaynag -t warning -m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your Wayland session and make everyone sad.' -b '*Sigh* Roger roger' 'swaymsg exit'
+  elif [[ "$1" == "river" ]]; then
+    exit
+  fi

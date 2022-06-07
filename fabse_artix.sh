563a89d6d597161f0a0de749665e16cefc2854a0
fabseman <fabse_utilities@pm.me>
2022-06-07 08:27:18 +0000

    Added "chaotic AUR" as repo

--- fabse_artix.sh
+++ fabse_artix.sh
@@ -14,6 +14,11 @@ EOF
 
 # Package-installation
   
+  doas pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+  doas pacman-key --lsign-key FBA220DFC880C036
+  doas pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+  doas cp configs/pacman.conf /etc/pacman.conf
+  doas pacman --noconfirm -Syu
   doas sed -i "s/mkinitcpio/mkinitcpio wireplumber/g" /etc/pacman.conf
   cd packages || exit
   WIREPLUMBER="$(ls -- *wireplumber-*)"

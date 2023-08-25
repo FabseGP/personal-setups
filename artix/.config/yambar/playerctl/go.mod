b885b515976091677e8adc1a5b132833f1ed11e0
fabseman <4969281-FabseGP02@users.noreply.gitlab.com>
2023-08-25 14:49:43 +0000

    Major refactoring

--- artix/.config/yambar/playerctl/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/and3rson/dotfiles/.config/yambar/playerctl
-
-go 1.17
-
-require github.com/godbus/dbus/v5 v5.0.6
--- artix/Scripts/pixelflasher.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-  git clone https://github.com/badabing2005/PixelFlasher.git
-  cd PixelFlasher || exit
-  pip3 install virtualenv wheel
-  python3 -m venv venv
-  . venv/bin/activate
-  pip3 install -r requirements.txt
-  ./build.sh

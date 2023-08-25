d0627310d6bb088daf5ec8d7eb5a8eaf0231f490
fabseman <4969281-FabseGP02@users.noreply.gitlab.com>
2023-08-25 15:19:40 +0000

    Added .local/bin

---
+++ artix/configs/.local/bin/logout.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+  if [[ "$1" == "sway" ]] && [[ $(pidof river) ]]; then
+    swaynag -t warning -m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your Wayland session and make everyone sad.' -b '*Sigh* Roger roger' 'swaymsg exit'
+  elif [[ "$1" == "river" ]] && [[ $(pidof sway) ]]; then
+    riverctl exit
+  else
+    for service in yambar foot pipewire mako wl-paste sunpaper sworkstyle syncthing; do
+      if [[ $(pidof $service) ]]; then
+        killall $service
+      fi
+    done
+    if [[ "$1" == "sway" ]]; then
+      swaynag -t warning -m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your Wayland session and make everyone sad.' -b '*Sigh* Roger roger' 'swaymsg exit'
+    elif [[ "$1" == "river" ]]; then
+      riverctl exit
+    fi
+  fi

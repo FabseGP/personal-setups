db4adf7e5a8a4095fb084b5661cf227169297f36
fabseman <4969281-FabseGP02@users.noreply.gitlab.com>
2023-08-25 17:13:36 +0000

    Updated packages

--- fabse_artix.sh
+++ fabse_artix.sh
@@ -22,16 +22,16 @@ EOF
     doas cp configs/pacman_with_chaotic.conf /etc/pacman.conf; fi
     
   doas pacman --noconfirm -Syu
-  yes | doas pacman -S mesa-tkg-git paru
+  yes | doas pacman -S --needed mesa-tkg-git paru hdf5-openmpi
   doas pacman --noconfirm --needed -S alacritty libreoffice-fresh pavucontrol playerctl lutris-git steam elinks sweet-gtk-theme-dark protonup-qt bluez-utils man-db \
                                       bitwarden easyeffects librewolf zathura-pdf-mupdf swappy candy-icons-git lolcat modprobed-db inetutils moc mpv fwupd sway npm \
                                       kmahjongg handlr i3status-rust swayidle swaybg clipman ttf-font-awesome lib32-gamemode figlet lib32-vkbasalt calf helix qt5ct \
                                       bemenu-wayland qt6-wayland kvantum-qt5 phonon-qt5-gstreamer pipewire-alsa mangohud libselinux android-udev lsp-plugins pass kdeconnect \
                                       pipewire-pulse zsh-theme-powerlevel10k zsh-autosuggestions protontricks-git docbook-xsl xmlto octave discord_arch_electron swww \
                                       zsh-syntax-highlighting shellcheck brightnessctl aisleriot vimiv-qt tela-icon-theme-git mako wofi cura-bin rpi-imager font-manager \
-                                      bsd-games jq gvfs-mtp wallutils tumbler xarchiver sway-launcher-desktop gamemode smartmontools swaylock-effects python-pyclip \
-                                      bottom ld-lsb xdg-desktop-portal-wlr wireplumber nemo-fileroller gendesk schedtool samba qt6ct dkms foot sshfs upscayl-bin \
-                                      python-pywal ethtool lib32-ocl-icd bcachefs-tools-git dupeguru dosbox reshade-shaders-git fzf blueman android-tools bat rclone \
+                                      bsd-games jq gvfs-mtp wallutils tumbler xarchiver sway-launcher-desktop gamemode smartmontools swaylock-effects python-pyclip wine-gecko \
+                                      bottom ld-lsb xdg-desktop-portal-wlr wireplumber nemo-fileroller gendesk schedtool samba qt6ct dkms foot sshfs upscayl-bin wine-staging \
+                                      python-pywal ethtool lib32-ocl-icd bcachefs-tools-git dupeguru dosbox reshade-shaders-git fzf blueman android-tools bat rclone wine-mono \
                                       unrar ttf-opensans libxcrypt-compat noto-fonts-emoji ttf-iosevka-nerd ventoy-bin llvm lsd wget patchutils mypaint s-tui nemo nnn \
                                       ttf-iosevka-nerd yambar-git bibata-rainbow-cursor-theme lib32-libva lib32-gtk3 lib32-gst-plugins-base-libs heroic-games-launcher-bin \
                                       ttf-meslo-nerd-font-powerlevel10k lib32-giflib lib32-mpg123 lib32-openal lib32-v4l-utils lib32-libxslt lzip libva-utils swtpm \
@@ -40,9 +40,9 @@ EOF
   if ! [[ "$MODE" == "MINIMAL" ]]; then
    # doas pacman --needed -S plasma plasma-wayland-session # 1,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,31,32,33,34,35,36,37,38,39,40,41,42,43,48,46,47
     doas pacman --noconfirm --needed -S virt-manager qemu bridge-utils dnsmasq nss-mdns gimp sagemath arduino-cli arduino-avr-core geogebra freecad-git \
-                                        qutebrowser betterbird elogind boost obs-studio vbam-wx blender kdenlive foliate gnuplot meson kicad-library stremio \
-                                        kicad-library-3d artools-base artools-pkg wine-staging texlive polkit-kde-agent kicad-git syncthing inkscape waydroid motrix-bin \
-                                        wine-gecko wine-mono podman-compose podman python-pipx cups-pdf cups-dinit avahi-dinit libvirt-dinit; fi
+                                        qutebrowser betterbird elogind boost obs-studio vbam-wx blender kdenlive foliate gnuplot meson kicad-library motrix-bin \
+                                        kicad-library-3d artools-base artools-pkg texlive polkit-kde-agent kicad-git syncthing inkscape waydroid podman-compose \
+                                        podman-compose podman python-pipx cups-pdf cups-dinit avahi-dinit libvirt-dinit; fi
 
   if grep -q Intel "/proc/cpuinfo"; then doas pacman --noconfirm --needed -S intel-undervolt-dinit; # Poor soul :(
   elif grep -q AMD "/proc/cpuinfo"; then doas pacman --noconfirm --needed -S zenpower3-dkms ryzenadj-git; fi
@@ -57,7 +57,7 @@ EOF
   paru --needed --noconfirm --useask -S sworkstyle otf-openmoji macchina-bin pipes.sh sunwait-git ydotool-git miru-bin tachidesk-sorayomi-bin \
                                         wayshot wl-gammarelay-rs rivercarro cbonsai bash-pipes bastet rtl8812au-dkms-git nuclear-player-bin \
                                         nudoku deezer-enhanced-bin river-noxwayland sweet-kde-git deemix cmst 8bitdo-ultimate-controller-udev \
-                                        catppuccin-frappe-grub-theme-git grub-theme-tela-color-2k-git
+                                        catppuccin-frappe-grub-theme-git grub-theme-tela-color-2k-git stremio-beta
                                         
   if ! [[ "$MODE" == "MINIMAL" ]]; then paru --needed --noconfirm --useask -S pcbdraw qucs-s; fi
   paru --noconfirm --useask -Syu
